6a234b2687dbcb7f38a0c826e825d7ca700ee2ee
Dan Persa <dan.persa@gmail.com>
2016-11-08 00:17:32 +0100

    Better title for stories

--- src/components/story/StoryPage.js
+++ src/components/story/StoryPage.js
@@ -13,47 +13,36 @@ class StoryPage extends React.Component {
   render() {
     return (
     <div className="main-container">
-      <section className="page-title page-title-4">
-          <Container>
-              <Row>
-                  <Col md={6}>
-                      <h3 className="uppercase mb0">{this.props.title}</h3>
-                  </Col>
-                  <Col md={6} someClass="text-right">
-                      <ol className="breadcrumb breadcrumb-2">
-                          <li>
-                              <a href="index.html">Home</a>
-                          </li>
-                          <li>
-                              <a href="#">Stories</a>
-                          </li>
-                          <li className="active">Project Title</li>
-                      </ol>
-                  </Col>
-              </Row>
-          </Container>
+      <section >
+        <Container>
+          <Row>
+            <Col sm={8} smOffset={2} someClass="text-center">
+              <h1 className="uppercase mb64 mb-xs-32">{this.props.title}</h1>
+            </Col>
+          </Row>
+        </Container>
       </section>
       <section className="pt0 pb40">
-          <Container>
-              <Row>
-                  <Col sm={3} xs={6} mbXs={24}>
-                      <span>Date:</span>
-                      <h6 className="uppercase mb0">Oct 15</h6>
-                  </Col>
-                  <Col sm={3} xs={6} mbXs={24}>
-                      <span>Tags:</span>
-                      <h6 className="uppercase mb0">seaside, casino, ruin</h6>
-                  </Col>
-                  <Col sm={3} xs={6} mbXs={24}>
-                      <span>Location:</span>
-                      <h6 className="uppercase mb0">Constanta, Romania</h6>
-                  </Col>
-                  <Col sm={3} xs={6} mbXs={24}>
-                      <span>Author:</span>
-                      <h6 className="uppercase mb0">Dan</h6>
-                  </Col>
-              </Row>
-          </Container>
+        <Container>
+          <Row>
+            <Col sm={3} xs={6} mbXs={24}>
+                <span>Date:</span>
+                <h6 className="uppercase mb0">Oct 15</h6>
+            </Col>
+            <Col sm={3} xs={6} mbXs={24}>
+                <span>Tags:</span>
+                <h6 className="uppercase mb0">seaside, casino, ruin</h6>
+            </Col>
+            <Col sm={3} xs={6} mbXs={24}>
+                <span>Location:</span>
+                <h6 className="uppercase mb0">Constanta, Romania</h6>
+            </Col>
+            <Col sm={3} xs={6} mbXs={24}>
+                <span>Author:</span>
+                <h6 className="uppercase mb0">Dan</h6>
+            </Col>
+          </Row>
+        </Container>
       </section>
 
       <FullImage src={this.props.logo}/>
