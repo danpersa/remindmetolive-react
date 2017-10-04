8d7ff8998ff8e9f286f6cfe9fc6e0e3d33230aa7
Dan Persa <dan.persa@gmail.com>
2017-10-04 11:45:10 +0200

    Make the main menu responsive

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,47 +1,39 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import MainMenuLinks from './MainMenuLinks';
 
 const MainMenu = () => {
   return (
-      <div className="nav-container">
-        <a id="top" />
-        <nav>
-            <div className="nav-utility">
-                <div className="module left">
-                    <i className="ti-email">&nbsp;</i>
-                    <span className="sub">contact@remindmetolive.com</span>
-                </div>
+    <div className="nav-container">
+      <a id="top" />
+      <nav>
+        <div className="nav-utility">
+          <div className="module left">
+            <i className="ti-email">&nbsp;</i>
+            <span className="sub">contact@remindmetolive.com</span>
+          </div>
+        </div>
+        <div className="nav-bar">
+            <div className="module left">
+              <Link to="/">
+                <img className="logo logo-light" alt="remindmetolive" src="/images/logo.svg" />
+                <img className="logo logo-dark" alt="remindmetolive" src="/images/logo.svg" />
+              </Link>
             </div>
-            <div className="nav-bar">
-                <div className="module left">
-                    <Link to="/">
-                        <img className="logo logo-light" alt="remindmetolive" src="/images/logo.svg" />
-                        <img className="logo logo-dark" alt="remindmetolive" src="/images/logo.svg" />
-                    </Link>
-                </div>
-                <div className="module widget-handle mobile-toggle right visible-sm visible-xs">
-                    <i className="ti-menu" />
-                </div>
-                <div className="module-group right">
-                    <div className="module left">
-                        <ul className="menu">
-                            <li>
-                              <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                              <Link to="/stories/">Stories</Link>
-                            </li>
-                            <li>
-                              <Link to="/about/">About</Link>
-                            </li>
-                            <li>
-                              <Link to="/contact/">Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            <div className="module-group right hidden-xs">
+              <div className="module left">
+                <MainMenuLinks />
+              </div>
             </div>
-        </nav>
+        </div>
+        <div className="nav-bar visible-xs">
+          <div className="module-group">
+            <div className="module left">
+              <MainMenuLinks />
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
