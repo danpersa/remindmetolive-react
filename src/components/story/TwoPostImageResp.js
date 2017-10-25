0273e37b6d92ee7a2d47986644e178a99a65daca
Dan Persa <dan.persa@gmail.com>
2017-10-25 10:27:32 +0200

    Use images with fixed width when showing two on a row.

--- src/components/story/TwoPostImageResp.js
+++ src/components/story/TwoPostImageResp.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 
 const TwoPostImageResp = (props) => {
   return (
-    <TwoImages src1={buildSrcResp(props.dirPath, "S", "image", props.number1)}
-               src2={buildSrcResp(props.dirPath, "S", "image", props.number2)} />
+    <TwoImages src1={buildSrcResp(props.dirPath, "SW", "image", props.number1)}
+               src2={buildSrcResp(props.dirPath, "SW", "image", props.number2)} />
   );
 };
 
