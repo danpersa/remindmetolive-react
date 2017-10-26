8ee7316235b8e1798edefe64b1397090b7bfe6a1
Dan Persa <dan.persa@gmail.com>
2017-10-26 21:52:04 +0200

    Fix tests

--- src/components/story/buildSrc.spec.js
+++ src/components/story/buildSrc.spec.js
@@ -2,6 +2,6 @@ import buildSrc from './buildSrc';
 
 describe('buildSrc', () => {
   it('should build the src', () => {
-    expect(buildSrc("the-dir/", "image", "01", "1200")).toEqual("the-dir/image-01-1200.jpg");
+    expect(buildSrc("/the-dir/", "image", "01", "1200")).toEqual("http://localhost:9090/the-dir/image-01-1200.jpg");
   });
 });
